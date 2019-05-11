1caf878ff627ff0ce36411d539591e6bb19861cd
Tomasz Maciej Nowak <tomek_n@o2.pl>
2019-05-11 19:37:33 +0200

    mvebu: align device names to vendor_device format
    Add vendors in device names and also rename few device names, for easier
    identyfying potential firmware to flash. The vendor and device string is
    mainly derived from model/compatipble string in dts from particular
    device, but since not all devices are well described, some of the renames
    follow marketing names.
    
    Signed-off-by: Tomasz Maciej Nowak <tomek_n@o2.pl>

--- target/linux/mvebu/image/cortex-a53.mk
+++ target/linux/mvebu/image/cortex-a53.mk
@@ -1,27 +1,19 @@
 ifeq ($(SUBTARGET),cortexa53)
 
-define Device/globalscale-espressobin
-  KERNEL_NAME := Image
-  KERNEL := kernel-bin
+define Device/globalscale_espressobin
+  $(call Device/Default-arm64)
   DEVICE_TITLE := ESPRESSObin (Marvell Armada 3700 Community Board)
-  IMAGES := sdcard.img.gz
-  IMAGE/sdcard.img.gz := boot-scr | boot-img-ext4 | sdcard-img-ext4 | gzip | append-metadata
   DEVICE_DTS := armada-3720-espressobin
-  DTS_DIR := $(DTS_DIR)/marvell
   SUPPORTED_DEVICES := globalscale,espressobin
 endef
-TARGET_DEVICES += globalscale-espressobin
+TARGET_DEVICES += globalscale_espressobin
 
-define Device/armada-3720-db
-  KERNEL_NAME := Image
-  KERNEL := kernel-bin
+define Device/marvell_armada-3720-db
+  $(call Device/Default-arm64)
   DEVICE_TITLE := Marvell Armada 3720 Development Board DB-88F3720-DDR3
-  IMAGES := sdcard.img.gz
-  IMAGE/sdcard.img.gz := boot-scr | boot-img-ext4 | sdcard-img-ext4 | gzip | append-metadata
   DEVICE_DTS := armada-3720-db
-  DTS_DIR := $(DTS_DIR)/marvell
   SUPPORTED_DEVICES := marvell,armada-3720-db
 endef
-TARGET_DEVICES += armada-3720-db
+TARGET_DEVICES += marvell_armada-3720-db
 
 endif
